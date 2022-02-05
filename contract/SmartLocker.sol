4a722128f0ead42830762465f7568f7579cee4d4
Hideyoshi Moriya <hide@piyo.jp>
2022-02-05 20:26:55 +0900

    Create deposit and withdraw function

---
+++ contract/SmartLocker.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.11;
+
+contract SmartLocker {
+
+    mapping (address => uint) deposits;
+
+    event Deposit(address sender, uint amount);
+    event Withdraw(address sender, uint amount);
+
+    function balance() public view returns (uint) {
+        return deposits[address(msg.sender)];
+    }
+
+    function deposit() public payable {
+        deposits[address(msg.sender)] += msg.value;
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    function withdraw() public {
+        uint amount = deposits[address(msg.sender)];
+        deposits[address(msg.sender)] = 0;
+        payable(msg.sender).transfer(amount);
+        emit Withdraw(msg.sender, amount);
+    }
+
+}
