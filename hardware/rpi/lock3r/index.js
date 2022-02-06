aae14e50512ab0f345ac6ff58a9d0cc237c3c9f1
Hideyoshi Moriya <hide@piyo.jp>
2022-02-06 16:46:55 +0900

    add GPIO operation

--- hardware/rpi/lock3r/index.js
+++ hardware/rpi/lock3r/index.js
@@ -1,4 +1,5 @@
 const LOCKER_ID = 0
+const UNLOCK_PIN = 36 // GPIO16
 
 // JSON credential file SHOULD NOT be added to open repo.
 const FIREBASE_CREDENTIAL_PATH = "./lock3r-firebase-adminsdk-credential.json"
@@ -10,6 +11,10 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 })
 db = admin.firestore()
+
+// Setup GPIO
+const gpio = require('rpi-gpio')
+gpio.setup(UNLOCK_PIN, gpio.DIR_OUT)
 
 // Called when lockers data is updated
 db.collection("lockers").doc(`${LOCKER_ID}`)
@@ -25,10 +30,18 @@ async function runLockerOperation(data) {
 
 	if (data.operation === "unlock") {
 		console.log("start unlock")
+		unlock()
 	}
 
 	// reset
 	await db.collection("lockers").doc(`${LOCKER_ID}`).set({
 		operation: ""
 	}, {merge: true})
+}
+
+function unlock() {
+    gpio.write(UNLOCK_PIN, true)
+    setTimeout(() => {
+        gpio.write(UNLOCK_PIN, false)
+    }, 500)
 }
